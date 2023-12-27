3662a37a4542b3df384654104c7fd085777ec1c9
Tomek <propsek@gmail.com>
2023-12-27 13:37:05 +0100

    Init_project

--- zakupy_powtórka.py
+++ zakupy_powtórka.py
@@ -6,5 +6,10 @@ shopping_dict = {
 total_items = 0
 
 for shop, items in shopping_dict.items():
-    shop = shop.capitalize() 
-    print(f"Idę do {shop} i kupuję tam: {(items)}.").capitalize()
+    shop = shop.upper() 
+    items = [item.upper() for item in items]  
+    print(f"Idę do {shop} i kupuję tam: {items}.")
+
+    total_items += len(items)
+
+print(f'W sumie kupuję {total_items} produktów.')
