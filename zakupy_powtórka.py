21013a44ef2951128bcf18a677b6ce28f25faa65
Tomek <propsek@gmail.com>
2023-12-27 13:33:28 +0100

    Init_project

--- zakupy_powtórka.py
+++ zakupy_powtórka.py
@@ -2,3 +2,9 @@ shopping_dict = {
     "piekarnia": ['chleb', 'pączek', 'bułki'],
     "warzywniak": ['marchew', 'seler', 'rukola']
 }
+
+total_items = 0
+
+for shop, items in shopping_dict.items():
+    shop = shop.capitalize() 
+    print(f"Idę do {shop} i kupuję tam: {(items)}.").capitalize()
